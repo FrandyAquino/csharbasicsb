f3c8159ea44cb5c5ad35148fffeae513c633a861
FrandyAquino <frandyjavieraquino13@gmail.com>
2024-09-04 22:55:56 -0400

    Agregando los ejercicios de clases

---
+++ Clases/Clases/LibroCalificaciones.cs
@@ -0,0 +1,12 @@
+﻿// Fig. 4.4: LibroCalificaciones.cs
+// Declaración de la clase con un método que tiene un parámetro.
+using System;
+
+public class LibroCalificaciones
+{
+    // Muestra un mensaje de bienvenida para el usuario del LibroCalificaciones
+    public void MostrarMensaje(string nombreCurso)
+    {
+        Console.WriteLine("¡Bienvenido al libro de calificaciones para\n{0}!", nombreCurso);
+    }  // fin del método MostrarMensaje
+}  // fin de la clase LibroCalificaciones
---
+++ Clases/Clases/LibroPruebaCalificaciones.cs
@@ -0,0 +1,22 @@
+﻿// Fig. 4.5: LibroPruebaCalificaciones.cs
+// Crea objeto LibroCalificaciones y pasa una cadena a su método MostrarMensaje.
+using System;
+
+public class LibroPruebaCalificaciones
+{
+    // El método Main comienza la ejecución del programa
+    public static void Main(string[] args)
+    {
+        // Crea un objeto LibroCalificaciones y lo asigna a miLibroCalificaciones
+        LibroCalificaciones miLibroCalificaciones = new LibroCalificaciones();
+
+        // Pide el nombre del curso y lo recibe como entrada
+        Console.WriteLine("Por favor escriba el nombre del curso:");
+        string nombreDelCurso = Console.ReadLine(); // Lee una línea de texto
+        Console.WriteLine();   // Imprime en pantalla una línea en blanco
+
+        // Llama al método MostrarMensaje de miLibroCalificaciones
+        // y pasa nombreDelCurso como argumento
+        miLibroCalificaciones.MostrarMensaje(nombreDelCurso);
+    }  // fin de Main
+}
